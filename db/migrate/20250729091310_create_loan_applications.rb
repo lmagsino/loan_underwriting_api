8f5a5a9f2ab6d61eaa29ebb85db63e241485664c
Leo Magsino Jr <leo.magsino819@gmail.com>
2025-07-31 20:18:56 +0800

    Update loan application table

--- db/migrate/20250729091310_create_loan_applications.rb
+++ db/migrate/20250729091310_create_loan_applications.rb
@@ -1,16 +1,20 @@
+# db/migrate/xxx_create_loan_applications.rb
 class CreateLoanApplications < ActiveRecord::Migration[7.1]
   def change
     create_table :loan_applications do |t|
       t.references :user, null: false, foreign_key: true
-      t.integer :loan_type
-      t.decimal :amount
-      t.integer :status
+      t.integer :loan_type, default: 0
+      t.decimal :amount, precision: 10, scale: 2, null: false
+      t.integer :status, default: 0
       t.integer :risk_score
       t.text :decision_reason
       t.datetime :applied_at
       t.datetime :decided_at
-
       t.timestamps
     end
+    
+    add_index :loan_applications, :status
+    add_index :loan_applications, :applied_at
+    add_index :loan_applications, [:user_id, :status]
   end
 end
