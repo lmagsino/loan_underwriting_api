d2adfd10aa8cfd04fff00877efebd5a0438ab9e3
Leo Magsino Jr <leo.magsino819@gmail.com>
2025-07-31 20:19:42 +0800

    Update document table

--- db/migrate/20250729091311_create_documents.rb
+++ db/migrate/20250729091311_create_documents.rb
@@ -1,14 +1,16 @@
+# db/migrate/xxx_create_documents.rb
 class CreateDocuments < ActiveRecord::Migration[7.1]
   def change
     create_table :documents do |t|
       t.references :loan_application, null: false, foreign_key: true
-      t.integer :document_type
-      t.string :file_url
-      t.string :original_filename
+      t.integer :document_type, null: false
+      t.string :file_url, null: false
+      t.string :original_filename, null: false
       t.integer :file_size
-      t.datetime :uploaded_at
-
+      t.datetime :uploaded_at, default: -> { 'CURRENT_TIMESTAMP' }
       t.timestamps
     end
+    
+    add_index :documents, :document_type
   end
 end
