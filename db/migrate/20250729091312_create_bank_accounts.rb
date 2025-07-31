b249b9779bfd97ee64498f2562942ae906e615e9
Leo Magsino Jr <leo.magsino819@gmail.com>
2025-07-31 20:21:19 +0800

    Update bank account table

--- db/migrate/20250729091312_create_bank_accounts.rb
+++ db/migrate/20250729091312_create_bank_accounts.rb
@@ -1,15 +1,18 @@
+# db/migrate/xxx_create_bank_accounts.rb  
 class CreateBankAccounts < ActiveRecord::Migration[7.1]
   def change
     create_table :bank_accounts do |t|
       t.references :user, null: false, foreign_key: true
       t.string :account_id
       t.string :institution_name
-      t.integer :account_type
-      t.decimal :balance
-      t.boolean :is_primary
+      t.integer :account_type, default: 0
+      t.decimal :balance, precision: 12, scale: 2
+      t.boolean :is_primary, default: false
       t.datetime :connected_at
-
       t.timestamps
     end
+    
+    add_index :bank_accounts, :account_id, unique: true
+    add_index :bank_accounts, [:user_id, :is_primary]
   end
 end
