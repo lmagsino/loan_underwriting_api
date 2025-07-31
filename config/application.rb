969affdb5238208321339a3fb18ad9c3341c67b2
Leo Magsino Jr <leo.magsino819@gmail.com>
2025-07-31 20:13:11 +0800

    Add config to application

--- config/application.rb
+++ config/application.rb
@@ -40,5 +40,8 @@ module LoanUnderwritingApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.session_store :disabled
+
+    config.middleware.use ActionDispatch::Flash
   end
 end
