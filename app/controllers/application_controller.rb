3dbdc7afa5bfadbdb9839c1abba604f0b10bea68
Leo Magsino Jr <leo.magsino819@gmail.com>
2025-08-01 17:43:01 +0800

    Create application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
+# app/controllers/application_controller.rb
 class ApplicationController < ActionController::API
+  before_action :authenticate_request
+  
+  attr_reader :current_user
+  
+  private
+  
+  def authenticate_request
+    @current_user = AuthorizeApiRequest.new(request.headers).call
+  rescue StandardError => e
+    render json: { error: e.message }, status: :unauthorized
+  end
+  
+  def authenticate_request!
+    render json: { error: 'Not authorized' }, status: :unauthorized unless current_user
+  end
 end
