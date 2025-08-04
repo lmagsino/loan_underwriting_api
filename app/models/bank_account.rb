bfeb45b62703a77c26a3536b449814c3d3b4f774
Leo Magsino Jr <leo.magsino819@gmail.com>
2025-08-04 17:44:39 +0800

    Update bank account model

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -1,3 +1,9 @@
 class BankAccount < ApplicationRecord
   belongs_to :user
+  has_many :transactions, dependent: :destroy
+  
+  validates :institution_name, presence: true
+  validates :account_id, uniqueness: true, allow_blank: true
+  
+  enum account_type: { checking: 0, savings: 1, credit: 2 }
 end
