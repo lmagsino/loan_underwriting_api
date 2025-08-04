d53d635fc122a395143deaa76b77241835a920c3
Leo Magsino Jr <leo.magsino819@gmail.com>
2025-08-04 17:44:13 +0800

    Update document model

--- app/models/document.rb
+++ app/models/document.rb
@@ -7,12 +7,10 @@ class Document < ApplicationRecord
   
   enum document_type: {
     identity: 0,
-    income_proof: 1,
+    income_proof: 1, 
     bank_statement: 2,
     employment_letter: 3,
     tax_return: 4,
     other: 5
   }
-  
-  scope :by_type, ->(type) { where(document_type: type) }
 end
