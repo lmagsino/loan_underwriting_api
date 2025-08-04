59630e9034dadd7c803d21c25922b229cb920832
Leo Magsino Jr <leo.magsino819@gmail.com>
2025-08-04 17:45:01 +0800

    Update transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,10 @@
 class Transaction < ApplicationRecord
   belongs_to :bank_account
+  
+  validates :amount, :date, presence: true
+  validates :amount, numericality: true
+  
+  scope :income, -> { where('amount > 0') }
+  scope :expenses, -> { where('amount < 0') }
+  scope :recent, -> { order(date: :desc) }
 end
